dd1a694af59667f9bc0a1d2a6df1d7ad0064e010
Tawishi <tawishisharma1@gmail.com>
2021-09-23 15:28:27 +0530

    Boiler-plate ready

--- tcp_client.cpp
+++ tcp_client.cpp
@@ -1,48 +1,54 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include<iostream>
 
-#include<sys/socket.h> //has APIs to create sockets
+#include<sys/socket.h> 
 #include<sys/types.h>
 
-#include<netinet/in.h> //has structure to store address information
-#include <unistd.h> // close()
-#include "encrypt.h" // header to encrypt message
+#include<netinet/in.h> 
+#include <unistd.h> 
+
+
+using namespace std;
 
 int main(int argc, char** argv) {
 	
 	// TCP Client
 
 	// Create a Socket
-	int network_socket; // socket descriptor
-
-	// AF_INET = domain of socket
-	// soc = type of socket; TCP socket (other is UDP socket)
-	// third is Protocol : 0=default=TCP(other is raw socket)
+	int network_socket;
 	network_socket = socket(AF_INET,SOCK_STREAM, 0);
 
 	// Structure for connection address
 	struct sockaddr_in server_address;
-	server_address.sin_family = AF_INET; // type/family of connection address
-	server_address.sin_port = htons(8080); // remote connection port -> integer port put in right network order
-	server_address.sin_addr.s_addr = INADDR_ANY; // actual server address - we connect to any IP on local
+	server_address.sin_family = AF_INET;
+	server_address.sin_port = htons(8080);
+	server_address.sin_addr.s_addr = INADDR_ANY; 
 
 	// Connect to Other Side
-	// returns integer as to if the connectin was made(0) or not
 	int connection_status = connect(network_socket, (struct sockaddr *) &server_address, sizeof(server_address));
-
 	if (connection_status == -1)
 		printf("There was an error making a connection to the remote server\n\n");
 
-	// Send encrypted message to Server
-	string str_server_message = encrypt(argc, argv);
-	char server_message[256];
+	// request public key
+	write(network_socket, "Please share the server Public key\n",sizeof("Please share the server Public key\n"));
 
-	for(int i=0;i<str_server_message.length();i++) {
-		server_message[i] = str_server_message[i];
-	}
+	// // Send encrypted message to Server
+	// string str_server_message = encrypt(argc, argv);
+	// char server_message[256];
 
-	send(network_socket, server_message, sizeof(server_message), 0);
-	
+	// for(int i=0;i<str_server_message.length();i++) {
+	// 	server_message[i] = str_server_message[i];
+	// }
+
+	// receive server public key
+	char message[255];
+	read(network_socket, &message, sizeof(message));
+	cout<<"Server sent the public key = \t"<<message<<"\n";
+
+	// send the digital evelope to server
+	write(network_socket, "Digital Envelope\n",sizeof("Digital Envelope\n"));
+
+	// send the symmetric encrypted message to server
+
 	// close socket
 	close(network_socket);
 
--- tcp_server.cpp
+++ tcp_server.cpp
@@ -1,49 +1,58 @@
 #include<iostream>
 #include<string>
 
-#include<sys/socket.h> //has APIs to create sockets
+#include<sys/socket.h> 
 #include<sys/types.h>
 
-#include<netinet/in.h> //has structure to store address information
-#include<unistd.h> // close()
-#include "decrypt.h"
+#include<netinet/in.h> 
+#include<unistd.h>
 
 using namespace std;
 
 int main() {
+
     // Create server socket
     int server_socket;
     server_socket = socket(AF_INET,SOCK_STREAM, 0);
 
     // define server address
     struct sockaddr_in server_address;
-    server_address.sin_family = AF_INET; // type/family of connection address
-	server_address.sin_port = htons(8080); // remote connection port -> integer port put in right network order
-	server_address.sin_addr.s_addr = INADDR_ANY; // actual server address - we connect to any IP on local
+    server_address.sin_family = AF_INET; 
+	server_address.sin_port = htons(8080);
+	server_address.sin_addr.s_addr = INADDR_ANY; 
 
     // bind socket to specified IP and port
     bind(server_socket, (struct sockaddr *) &server_address, sizeof(server_address));
 
     //listen for connections
-    // 1 arg - server socket
-    // 2 arg - backlog: how many connections can be waiting for the socket
     listen(server_socket, 1);
 
     // store the client socket when we listen and then accept the connection
     int client_socket;
-    // 1-socket for server
-    // 2-structure for address for client
-    // 3-size for address of client
     client_socket = accept(server_socket, NULL, NULL);
 
     // Receive data from Client
 	char client_message[256];
-	recv(client_socket, &client_message, sizeof(client_message), 0);
+    read(client_socket, &client_message, sizeof(client_message));
+	printf("Client message - %s\n ", client_message);
 
-	// print the data
-    string plaintext = decrypt(client_message);
-	printf("The client sent the message - %s\nwhich translates to ", client_message);
-    cout<<plaintext<<"\n\n";
+    // send public key
+    send(client_socket, "Public key =\n", 15,0);
+    cout<<"Public key shared with client\n\n";
+
+    // read the envelope from client
+    char buffer[256];
+    auto n = read(client_socket, buffer,255);
+    if (n < 0) 
+        printf("ERROR reading from socket");
+    printf("Digital envelope by client : %s\n",buffer);
+
+    // decrypt message Symmetric key and display
+    char message[255];
+	read(client_socket, &message, sizeof(message));
+	cout<<"Client message = "<<message<<"\n";
+
+    // receive the encrypted message from client
 
     //close socket
     close(server_socket);
